10b116470696d0bd694b581ffe81c99bbd420f56
Copi5336 <jason.han@tpisoftware.com>
2021-06-02 15:10:55 +0800

    no message

--- src/store/index.js
+++ src/store/index.js
@@ -2,15 +2,46 @@ import { createStore } from 'vuex'
 
 // Create a new store instance.
 const store = createStore({
-  state () {
+  state() {
     return {
       count: 0,
-      name:'jason'
+      name: 'jason',
+      token: 'qd281928d8ji',
+      price: 199,
+      saleprice: 500,
+      users: []
+    }
+  },
+  getters: {
+    sale80(state) {
+      return state.price * 0.8
+    },
+    sale50(state) {
+      return state.price * 0.5
     }
   },
   mutations: {
-    increment (state) {
-      state.count++
+    editprice(state, payload) {
+      state.price = payload
+    },
+    editsaleprice(state, payload) {
+      state.saleprice = payload
+    },
+    setuserlist(state, payload) {
+      state.users = payload
+    }
+  },
+  actions: {
+    getUserlist(context) {
+      fetch(`https://5f8a534d18c33c0016b31610.mockapi.io/vuex`).then(res => res.json()).then(data => {
+        context.commit('setuserlist', data)
+      })
+    },
+    getUserid(context, payload) {
+      const { id } = payload
+      fetch(`https://5f8a534d18c33c0016b31610.mockapi.io/vuex/${id}`).then(res => res.json()).then(data => {
+        context.commit('setuserlist', data)
+      })
     }
   }
 })
--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,24 +1,33 @@
 <template>
   <div>Login</div>
-  {{getname}}
+  {{ name }}
+  {{ token }}
+  <p>原價 {{ saleprice }} 特價{{ sale80 }}</p>
+  <button @click="editprice(60)">修改售價</button>
+  <button @click="editsaleprice(360)">修改原價</button>
 </template>
 
 <script>
+import { mapState, mapGetters, mapMutations, mapActions } from "vuex";
 export default {
+  created() {
+    this.getUserlist();
+    this.getUserid({ id: 1 });
+  },
   data: () => ({
-    count: 0  
+    count: 0,
   }),
   methods: {
+    ...mapMutations(["editsaleprice", "editprice"]),
+    ...mapActions(["getUserlist", "getUserid"]),
     inc() {
       this.count++;
-    }
+    },
   },
-  computed:{
-    getname(){
-      console.log(this.$store);
-      return this.$store.state.name
-    }
-  }
+  computed: {
+    ...mapState(["name", "token", "saleprice"]),
+    ...mapGetters(["sale80", "sale50"]),
+  },
 };
 </script>
 
