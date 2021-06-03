7542e82df6508cbcc3233bff991da16ca7711a1a
Copi5336 <jason.han@tpisoftware.com>
2021-06-03 15:35:36 +0800

    add api helper

--- src/views/example/emptyVue/index.vue
+++ src/views/example/emptyVue/index.vue
@@ -4,9 +4,7 @@
 
 <script>
 export default {
-  created() {
-    
-  },
+  created() {},
   data: () => ({
     count: 0,
   }),
@@ -18,4 +16,52 @@ export default {
 };
 </script>
 
-<style></style>
+<style lang="scss">
+.content {
+  display: block;
+  position: fixed;
+  overflow: hidden;
+  width: 100%;
+  height: 100vh;
+  * {
+    box-sizing: border-box;
+  }
+  > div {
+    float: left;
+  }
+}
+
+.aside-menu {
+  width: 25%;
+  height: 100%;
+  border-right: 1px solid #aaa;
+  background-color: #f1f1f1;
+  overflow-y: scroll;
+  label {
+    position: relative;
+    margin-right: 2em;
+    display: block;
+    font-size: 1.2rem;
+    line-height: 2;
+  }
+  select {
+    position: absolute;
+    margin-top: 0.3rem;
+    left: 6rem;
+    font-size: 1.2rem;
+    width: auto;
+    min-width: 110px;
+  }
+  input {
+    padding: 2px 8px;
+    font-size: 1.2rem;
+    line-height: 2;
+    width: 110%;
+  }
+}
+
+.wraps {
+  padding: 1em;
+  border-bottom: 1px solid #666;
+}
+</style>
