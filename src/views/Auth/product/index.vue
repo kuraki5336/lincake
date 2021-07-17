16173f1f0d5a486a67474e4a31ec81586a30fd8e
Copi5336 <jason.han@tpisoftware.com>
2021-07-17 13:32:37 +0800

    no message

---
+++ src/views/Auth/product/index.vue
@@ -0,0 +1,136 @@
+<template>
+  <div>
+    <div class="products-details">
+      <div class="products-details__inner-container">
+        <div class="products-item">
+          <h2>12345678</h2>
+          <div class="details">
+            <div class="detail_left">
+              <img :src="'products/' + FAstitle + '/' + FItemno + '.jpg'" />
+            </div>
+            <div class="detail_right">
+              <div class="detail_right__content">
+                <p>
+                  山形食パンに比べて中身のきめが細かく、もっちりとした食感で、トーストしても食べ応えがあります。
+                  サンドウィッチにすれば、具材との食感のバランスは最高です。
+                </p>
+                <div class="detail_right__composition">
+                  <van-row>
+                    <van-col span="6">材料</van-col>
+                    <van-col span="18">@糖 @小麥 @蛋素</van-col>
+                  </van-row>
+                  <van-row>
+                    <van-col span="6">售價</van-col>
+                    <van-col span="18">100元</van-col>
+                  </van-row>
+                </div>
+
+                <div class="detail_right__nutrition">
+                  <van-row>
+                    <van-col span="6">熱量</van-col>
+                    <van-col span="6">600kul</van-col>
+                    <van-col span="6">脂肪</van-col>
+                    <van-col span="6">377emb</van-col>
+                  </van-row>
+                  <van-row>
+                    <van-col span="6">碳水化合物</van-col>
+                    <van-col span="6">147mm</van-col>
+                    <van-col span="6">糖</van-col>
+                    <van-col span="6">28g</van-col>
+                  </van-row>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="similar_product"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { onMounted } from "vue";
+export default {
+  setup() {
+    onMounted(() => {});
+  },
+  data: () => ({
+    FAstitle: "",
+    FItemno: "",
+  }),
+  mounted() {
+    /* 选项 API */
+    this.FAstitle = this.$route.query.astitle;
+    this.FItemno = this.$route.query.itemno;
+  },
+  methods: {},
+};
+</script>
+
+<style lang="scss">
+.products-details {
+  max-width: 980px;
+  margin: auto;
+}
+.products-item {
+  padding-right: 15px;
+  padding-left: 15px;
+  margin-right: auto;
+  margin-left: auto;
+  border-bottom: solid 3px #0d0057;
+  border-top: solid 3px #0d0057;
+  margin-top: 80px;
+  padding: 30px 20px;
+}
+
+.details {
+  box-sizing: border-box;
+
+  .detail_left {
+    float: left;
+    width: 50%;
+
+    img {
+      width: 100%;
+    }
+  }
+
+  .detail_right {
+    float: right;
+    width: 50%;
+
+    &__content {
+      padding: 0 30px;
+
+      p {
+        margin-bottom: 1.2em;
+        line-height: 1.9;
+      }
+    }
+
+    &__composition {
+      margin-top: 20px;
+    }
+
+    &__nutrition {
+      margin-top: 20px;
+    }
+  }
+
+  .van-col {
+    line-height: 2rem;
+    text-align: center;
+    margin-bottom: 1px;
+  }
+
+  .van-col:nth-child(even) {
+    background-color: #e7e7e7;
+  }
+
+  .van-col:nth-child(odd) {
+    color: #fff;
+    background-color: #8e8e8e;
+  }
+}
+</style>
---
+++ src/views/Auth/products/index.vue
@@ -0,0 +1,109 @@
+<template>
+  <div>
+    <div
+      v-for="(products, index) in plu_items"
+      :key="index"
+      class="tab_content products"
+    >
+      <h2>{{ products.title }}</h2>
+      <ul>
+        <li
+          v-for="(plu, index) in products.items"
+          :key="index"
+          @click="dopluclick(products.astitle, plu.itemno)"
+        >
+          <a>
+            <img :src="products.astitle + '/' + plu.itemno + '.jpg'" />
+          </a>
+          <p>{{ plu.name }}</p>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {},
+  data: () => ({
+    plu_items: [
+      {
+        title: "餅乾",
+        astitle: "cook",
+        items: [
+          {
+            itemno: "001",
+            name: "伯爵紅茶",
+          },
+          {
+            itemno: "002",
+            name: "草莓心",
+          },
+          {
+            itemno: "003",
+            name: "肉桂杏仁",
+          },
+          {
+            itemno: "004",
+            name: "肉桂核桃",
+          },
+          {
+            itemno: "005",
+            name: "椰香牛奶",
+          },
+          {
+            itemno: "006",
+            name: "宇治抹茶",
+          },
+          {
+            itemno: "007",
+            name: "蔓越莓餅乾",
+          },
+          {
+            itemno: "008",
+            name: "芝麻餅乾",
+          },
+          {
+            itemno: "008",
+            name: "杏仁巧克力",
+          },
+        ],
+      },
+      {
+        title: "雪Q餅",
+        astitle: "snow",
+        items: [
+          {
+            itemno: "001",
+            name: "蔓越莓雪Q餅",
+          },
+          {
+            itemno: "002",
+            name: "抹茶蔓越莓雪Q餅",
+          },
+        ],
+      },
+      {
+        title: "船餅",
+        astitle: "ship",
+        items: [
+          {
+            itemno: "001",
+            name: "法式焦糖蜂蜜杏仁船餅",
+          },
+        ],
+      },
+    ],
+  }),
+  methods: {
+    dopluclick(title, no) {
+      this.$router.push({
+        path: "/product",
+        query: { astitle: title, itemno: no },
+      });
+    },
+  },
+};
+</script>
+
+<style lang="scss"></style>
